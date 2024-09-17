a15c2b905fd6495812b4248a79e2b7dda3701f2f
tuan-cre <lhtuan7924@gmail.com>
2024-09-17 08:55:00 +0700

    bt2 sua

--- Chuong 2/bt2.py
+++ Chuong 2/bt2.py
@@ -2,4 +2,9 @@ t=int(input("Nhập số giây:"))
 hour=(t//3600)%24
 minute=(t%3600)//60
 second=(t%3600)%60
-print(hour,":",minute,":",second)
+time="AM"
+if hour>=0 and hour<=12:
+    time="AM"
+else:
+    time="PM"
+print(hour,":",minute,":",second,"",time)

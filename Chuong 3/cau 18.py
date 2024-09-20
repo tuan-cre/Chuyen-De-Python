89afd79e30e5d3865fa00c3fbb37adefb00cdfd9
tuan-cre <lhtuan7924@gmail.com>
2024-09-20 16:46:35 +0700

    cau 18

--- Chuong 3/cau 18.py
+++ Chuong 3/cau 18.py
@@ -5,3 +5,7 @@ for i in range(n):
         print("* " * n)
     else:
         print("* " + "  " * (n-2) + "*")
+#ve tam giac vuong bang dau * voi chieu dai 2 canh goc vuong la m
+m = int(input("Nhập chiều dài 2 cạnh góc vuông của tam giác vuông: "))
+for i in range(1, m+1):
+    print("* " * i)
